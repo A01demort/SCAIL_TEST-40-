4a2863692a51511ebdc5b5111f3b38ffadc61ae1
A01demort <150997726+A01demort@users.noreply.github.com>
2026-01-02 15:19:49 +0900

    Update Wan2.1_Vace_a1.sh

--- Wan2.1_Vace_a1.sh
+++ Wan2.1_Vace_a1.sh
@@ -195,7 +195,7 @@ else
   echo -e "\n====🎓 AI 교육 & 커뮤니티 안내====\n"
   echo -e "1. Youtube : https://www.youtube.com/@A01demort"
   echo "2. 교육 문의 : https://a01demort.com"
-  echo "3. Udemy 강의 : https://bit.ly/comfyclass"
+  echo "3. CLASSU 강의 : https://classu.co.kr/me/19375"
   echo "4. Stable AI KOREA : https://cafe.naver.com/sdfkorea"
   echo "5. 카카오톡 오픈채팅방 : https://open.kakao.com/o/gxvpv2Mf"
   echo "6. CIVITAI : https://civitai.com/user/a01demort"
